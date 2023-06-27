d78342ada72be584751039a6cae1de0cc39078f3
hsherrod2019 <hsherrod2019@gmail.com>
2023-06-27 13:48:36 -0700

    Updated with codes 1, 3, and 5 from week 2

--- R/MyRFunctions.R
+++ R/MyRFunctions.R
@@ -1,13 +1,37 @@
-library(devtools)
+## Code 1: Calculating average wavenumber
+data(raman_hdpe, package = "OpenSpecy")
 
-use_r("hello") #will create a file called R/hello.R
+calculate_average_wavenumber <- function(raman_hdpe) {
+  if (nrow(raman_hdpe) == 0) {
+    stop("No data available. Please provide Raman spectra data.")
+  }
 
-hello <- function(name) {
-  message <- paste0("Hello ", name, "!")
-
-  return(message)
+  average_wavenumber <- mean(raman_hdpe$wavenumber, na.rm = TRUE)
+  return(average_wavenumber)
 }
 
-hello("Hannah")
+print_average_wavenumber <- function(average_wavenumber) {
+  cat("Average Wavenumber:", average_wavenumber, "\n")
+}
+
+average_wavenumber <- calculate_average_wavenumber(raman_hdpe)
+print_average_wavenumber(average_wavenumber)
 
 
+## Code 3: Calculating intensity ratio
+calculate_intensity_ratio <- function(peak1_intensity, peak2_intensity) {
+  ratio <- peak1_intensity/peak2_intensity
+  return(ratio)
+}
+
+
+## Code 5: Calculating average intensity
+data(raman_hdpe, package = "OpenSpecy")
+
+calculate_average_intensity <- function(raman_hdpe) {
+  average_intensity <- mean(raman_hdpe$intensity)
+  return(average_intensity)
+}
+
+average_intensity <- calculate_average_intensity(raman_hdpe)
+print(paste("Average Intensity:", average_intensity))

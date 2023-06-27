47592d1e7584a950fb245e1707c7888c0deb3f76
hsherrod2019 <hsherrod2019@gmail.com>
2023-06-27 13:44:35 -0700

    Updating github with my R functions.

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,13 @@
-load_all()
+library(devtools)
+
+use_r("hello") #will create a file called R/hello.R
+
+hello <- function(name) {
+  message <- paste0("Hello ", name, "!")
+
+  return(message)
+}
+
 hello("Hannah")
+
 

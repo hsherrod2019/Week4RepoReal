a7f5c562f552e70d7dd64c1a237cf907f222dcd7
hsherrod2019 <hsherrod2019@gmail.com>
2023-06-27 13:25:03 -0700

    Testing "hello" function

---
+++ R/Overwritedversion.R
@@ -0,0 +1,11 @@
+library(devtools)
+
+use_r("hello") #will create a file called R/hello.R
+
+hello <- function(name) {
+  message <- paste0("Hello ", name, "!")
+
+  return(message)
+}
+
+hello("Hannah")

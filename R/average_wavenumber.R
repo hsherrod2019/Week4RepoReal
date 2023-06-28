2c56962dbd9ed362b7c8571448b6bc0925c270a4
hsherrod2019 <hsherrod2019@gmail.com>
2023-06-28 11:32:59 -0700

    Reorganizing the files so that the folders are accessible for load_all() function.

---
+++ R/average_wavenumber.R
@@ -0,0 +1,33 @@
+if (!require(lattice, quietly = TRUE)) {
+  install.packages("lattice")
+  library(lattice)
+}
+
+if(!require(OpenSpecy, quietly = TRUE)) {
+  install.packages("OpenSpecy")
+  library(OpenSpecy)
+}
+
+#' Calculating average wavenumber
+#'
+#' @param raman_hdpe
+#'
+#' @return Printing the average wavenumber
+#' @export
+#'
+#' @examples Values from the raman_hdpe data set
+calculate_average_wavenumber <- function(raman_hdpe) {
+  if (nrow(raman_hdpe) == 0) {
+    stop("No data available. Please provide Raman spectra data.")
+  }
+
+  average_wavenumber <- mean(raman_hdpe$wavenumber, na.rm = TRUE)
+  return(average_wavenumber)
+}
+
+print_average_wavenumber <- function(average_wavenumber) {
+  cat("Average Wavenumber:", average_wavenumber, "\n")
+}
+
+average_wavenumber <- calculate_average_wavenumber(raman_hdpe)
+print_average_wavenumber(average_wavenumber)
--- Week4main.R
+++ Week4main.R
@@ -3,3 +3,8 @@ library(devtools)
 create_package("~/Documents/Week4RPackage/Week4RepoReal")
 
 use_git()
+
+#myfunction1
+use_r("average_wavenumber")
+
+load_all()

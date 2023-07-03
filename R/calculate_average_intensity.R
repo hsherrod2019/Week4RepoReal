c8a195b28520c3618d1328e959473918870f7bf5
hsherrod2019 <hsherrod2019@gmail.com>
2023-07-03 10:55:30 -0700

    Updating calculate_average_intensity example

--- R/calculate_average_intensity.R
+++ R/calculate_average_intensity.R
@@ -1,19 +1,16 @@
-data(raman_hdpe, package = "OpenSpecy")
-
 #' Calculate Average Intensity
 #'
-#' @param raman_hdpe Raman spectra data set; feel free to choose your own
+#' @param raman_hdpe Raman spectra data set; an example data set
 #'
 #' @return Average intensity of entire data set
 #' @export
 #'
 #' @examples
-#' calculate_average_intensity <- function(your_data) `...`
+#' data(raman_hdpe, package = "OpenSpecy")
+#' calculate_average_intensity(raman_hdpe)
 #'
 calculate_average_intensity <- function(raman_hdpe) {
   average_intensity <- mean(raman_hdpe$intensity)
   return(average_intensity)
 }
 
-average_intensity <- calculate_average_intensity(raman_hdpe)
-print(paste("Average Intensity:", average_intensity))
